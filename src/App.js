3d347588ad8c9ed83e30bc76172b375e916b8d12
Hayley Dixon <hdixon37@gmail.com>
2021-09-16 21:51:06 -0600

    Import Images component and render to page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { fetchData, fetchMarsData } from './apiCalls'
+import Images from './Images'
 
 class App extends Component {
   constructor() {
@@ -14,11 +15,21 @@ class App extends Component {
   componentDidMount = () => {
     this.setState({ isLoading: true})
     fetchMarsData()
-      .then(data => this.setState({ spaceData:data.photos }))
+      .then(data => this.setState({ spaceData:data.photos, isLoading: false }))
   }
   render() {
+    const { spaceData, error, isLoading } = this.state;
+    console.log(spaceData)
     return (
-      <h1>Spacestagram</h1>
+      <main className='App'>
+        <h1>Spacestagram</h1>
+        <h2>Brought to you by NASA's image API</h2>
+        {isLoading && <p className='loading-page'> Please hold, gathering data </p>}
+        {error && <p className='error-page'> Sorry, can't access space rn. Please come back later</p>}
+          <Images id='images'
+          images={spaceData}
+        />
+      </main>
     )
   }
 }
