66c818545d2b100fb19818254da1097a013fb667
Hayley Dixon <hdixon37@gmail.com>
2021-09-16 21:10:10 -0600

    Create state and fetch API data and store in state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import { fetchData, fetchMarsData } from './apiCalls'
 
 class App extends Component {
   constructor() {
-    super()
+    super();
+      this.state= {
+        spaceData: [],
+        error: '',
+        isLoading: false
+      }
+  }
+  componentDidMount = () => {
+    this.setState({ isLoading: true})
+    fetchMarsData()
+      .then(data => this.setState({ spaceData:data.photos }))
   }
   render() {
     return (
