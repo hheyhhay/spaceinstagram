e9dce20ea3ddb0338816c3630ffcb8878af69d28
Hayley Dixon <hdixon37@gmail.com>
2021-09-18 12:43:06 -0600

    Add Alt text

--- src/Images.js
+++ src/Images.js
@@ -6,24 +6,19 @@ const Images = ( { images } ) => {
   const imageCards = images.map(image => {
     return (
       <div className='image-card' key={image['id']}>
-        <img className='space-image'  src={image['img_src']} />
+        <img className='space-image'  src={image['img_src']} alt={`Landing of ${image.rover['name']} on Mars`}/>
         <h3 className='camera-name'>{image.rover['name']} Rover - {image.camera['full_name']}</h3>
         <h2 className='capture-date'>{ image['earth_date'] }</h2>
-        <button>Like</button>
+        <button className='like-button'>Like</button>
       </div>
     )
   })
+
   return (
     <div className='image-display'>
       {imageCards.length > 0 && <div className='image-container'>{imageCards}</div> }
     </div>
   )
-
 }
 
 export default Images;
-
-
-//Curiousity rover - Front Havard Avoidance Camera
-//2021 - 09-01
-//like
