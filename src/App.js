dc5521530eeca52cb114f8df25ad9781e75be59b
Hayley Dixon <hdixon37@gmail.com>
2021-09-19 11:29:09 -0600

    Refactor App class to use Hooks

--- src/App.js
+++ src/App.js
@@ -1,70 +1,65 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { fetchMarsData } from './apiCalls'
 import Images from './Images'
 
 
-class App extends Component {
-  constructor() {
-    super();
-      this.state= {
-        spaceData: [],
-        savedFavorites: [],
-        error: '',
-        isLoading: false
+// class App extends Component {
+//   constructor() {
+//     super();
+//       this.state= {
+//         spaceData: [],
+//         savedFavorites: [],
+//         error: '',
+//         isLoading: false
+//       }
+//   }
+const App = () => {
+    const [spaceData, setSpaceData] = useState([])
+    const [error, setError] = useState('');
+    const [isLoading, setIsLoading ] = useState(true)
+
+    const getData = async () => {
+      try {
+        const data = await fetchMarsData()
+        setSpaceData(data.photos)
+        setIsLoading(false)
       }
-  }
-  // function App() {
-  //   const [spaceData, setSpaceData] = useState([])
-  //   const [error, setError] = useState('');
-  //   const [isLoading, setIsLoading ] = useState(false)
-  //
-  //   useEffect(() => {
-  //     const getData = fetchMarsData()
-  //     setStateData(getData.photos)
-  //
-  //   }
-  // }
-  componentDidMount = () => {
-    this.setState({ isLoading: true})
-    fetchMarsData()
-      .then(data => this.setState({ spaceData:data.photos, isLoading: false }))
-  }
-
-  handleClick = (id) => {
-    // console.log(this.props)
-    // console.log(this.state)
-    const favoriteImage = this.state.spaceData.find(image => image.id === id)
-    const filteredData = this.state.spaceData.filter(image => image.id !== id)
-
-    this.setState({ spaceData: filteredData})
-
-    if (!this.state.savedFavorites) {
-      this.setState({ savedFavorites:[favoriteImage]})
-    } else {
-      this.setState({ savedFavorites:[...this.state.savedFavorites, favoriteImage]})
+      catch (error) {
+        setError(error.message)
+      }
     }
+
+    useEffect( () => {
+      getData()
+    }, [] )
+
+const handleClick = (id) => {
+    return null
+    // const favoriteImage = this.state.spaceData.find(image => image.id === id)
+    // const filteredData = this.state.spaceData.filter(image => image.id !== id)
+    //
+    // this.setState({ spaceData: filteredData})
+    //
+    // if (!this.state.savedFavorites) {
+    //   this.setState({ savedFavorites:[favoriteImage]})
+    // } else {
+    //   this.setState({ savedFavorites:[...this.state.savedFavorites, favoriteImage]})
+    // }
   }
 
-
-
-
-  render() {
-    const { spaceData, savedFavorites, error, isLoading } = this.state;
     return (
       <main className='App'>
         <h1 className='header'>Spacestagram</h1>
         <h2 className='subheader'>Brought to you by NASA's image API</h2>
-        {isLoading && <p className='loading-page'> Please hold, gathering data </p>}
-        {error && <p className='error-page'> Sorry, can't access space rn. Please come back later</p>}
+        { isLoading && <p className='loading-page'> Please hold, gathering data </p> }
+        { error && <p className='error-page'> Sorry, can't access space rn. Please come back later</p> }
         <Images id='images'
-          images={spaceData}
-          favorites={ savedFavorites }
-          handleClick={ this.handleClick }
+          images={ spaceData }
+          handleClick={ handleClick }
         />
       </main>
     )
-  }
 }
 
 export default App;
