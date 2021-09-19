3669cbf442f44e1ffd42be2e27a0ed6a35adf9ed
Hayley Dixon <hdixon37@gmail.com>
2021-09-19 15:56:25 -0600

    Remove event handler prop

--- src/Images.js
+++ src/Images.js
@@ -1,51 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Images.css';
 import Card from './Card';
 
-const Images = ( { images, favorites, handleClick } ) => {
+const Images = ( { images} ) => {
 
-let favoriteCards;
-
-  if (!favorites) {
-    favoriteCards = []
-  } else {
-    favoriteCards = favorites.map(image => {
-      return (
-        <Card
-          key={ image['id']}
-          id={image['id']}
-          image={image['img_src']}
-          name={image.rover['name']}
-          cameraName={image.camera['full_name']}
-          date={ image['earth_date']}
-          favorited={true}
-          handleClick={handleClick}
-        />
-      )
-    }
-  )
-}
   const imageCards = images.map(image => {
-    // console.log(image.id, image['img_src'], image.camera['full_name'], image['earth_date'])
     return (
       <Card
-        key={ image['id']}
-        id={image['id']}
-        image={image['img_src']}
-        name={image.rover['name']}
-        cameraName={image.camera['full_name']}
-        date={ image['earth_date']}
-        favorited={false}
-        handleClick={handleClick}
+        key={ image.id }
+        id={ image.id }
+        image={ image.image }
+        name={ image.name }
+        cameraName={ image.cameraName }
+        date={ image.date}
       />
     )
-
   })
 
   return (
     <div className='image-display'>
       {imageCards.length > 0 && <div className='image-container'>
-      {favoriteCards}
       { imageCards }
       </div> }
     </div>
