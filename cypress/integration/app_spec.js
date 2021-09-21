797c1e5c7f8cecd82d9a08b745a4af643af4bf13
Hayley Dixon <hdixon37@gmail.com>
2021-09-21 08:59:07 -0600

    Finialize cypress testing

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -8,10 +8,6 @@ describe('Space images web application', () => {
   })
 
   it('User should be able to like the image', () => {
-    // cy.get('.image-card').first().get('.like-button').click()
-    // cy.get('[id=102693]').contains('.like-button').click()
-    // cy.contains('2015-05-30').click()
-    // cy.contains('2015-05-30').should('')
     cy.get('#102693').within(() => {
       cy.get('.like-button').click()
     })
